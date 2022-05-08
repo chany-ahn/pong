74d1db05647077f45d1ce9f4c1ceacda8f509b6b
chany-ahn <chany.kima@gmail.com>
2022-05-07 20:09:12 -0400

    paddle movement works

--- src/game.py
+++ src/game.py
@@ -30,7 +30,7 @@ class Game:
         x,y = self.ball.get_position()
         r = self.ball.r
 
-    # check collision with paddles
+        # check collision with paddles
         if x - r == self.paddle1.x + self.paddle1.w//2 and y <= self.paddle1.y + self.paddle1.l and y >= self.paddle1.y - self.paddle1.l:
             self.ball.dx = -self.ball.dx
         
@@ -45,20 +45,38 @@ class Game:
         if x == r or x == self.w -r:
             self.ball.dx = -self.ball.dx
 
-        
-
-    
+    # function used to reset the game
     def reset_game(self):
         init_velocity = random.choice([[1,1],[1,-1],[-1,1],[-1,-1]])
 
         self.ball.set_velocity(init_velocity[0], init_velocity[1])
 
         self.score = (0,0)
+    
+    # movement of each paddle
+    def move_paddle1(self, addY):
 
-    def move_paddles(self):
+        if self.paddle1.y-self.paddle1.l//2 > 0 and self.paddle1.y+self.paddle1.l//2 < self.h:
+            self.paddle1.move(addY)
         
-        pass
+        if self.paddle1.y-self.paddle1.l//2 == 0 and addY > 0:
+            self.paddle1.move(addY)
 
+        if self.paddle1.y+self.paddle1.l//2 == self.h and addY < 0:
+            self.paddle1.move(addY)
+    
+
+    def move_paddle2(self, addY):
+        if self.paddle2.y - self.paddle2.l // 2 > 0 and self.paddle2.y+self.paddle2.l//2 < self.h:
+            self.paddle2.move(addY)
+
+        if self.paddle2.y-self.paddle2.l//2 == 0 and addY > 0:
+            self.paddle2.move(addY)
+
+        if self.paddle2.y+self.paddle2.l//2 == self.h and addY < 0:
+            self.paddle2.move(addY)
+        
+    # update per game loop
     def update(self):
         self.ball.move()
         self.collision()
--- src/main.py
+++ src/main.py
@@ -15,6 +15,8 @@ def main():
     g.reset_game()
     paddle1 = Rect(g.paddle1.x, g.paddle1.y, g.paddle1.w, g.paddle1.l)
     paddle2 = Rect(g.paddle2.x, g.paddle2.y, g.paddle2.w, g.paddle2.l)
+    
+    pygame.key.set_repeat(1,1)
 
     while 1:
         for event in pygame.event.get():
@@ -24,13 +26,24 @@ def main():
         paddle1.center = (g.paddle1.x, g.paddle1.y)
         paddle2.center = (g.paddle2.x, g.paddle2.y)
 
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_DOWN]:
+            g.move_paddle2(1)
+        if keys[pygame.K_UP]:
+            g.move_paddle2(-1)
+        if keys[pygame.K_s]:
+            g.move_paddle1(1)
+        if keys[pygame.K_w]:
+            g.move_paddle1(-1)
+
 
         screen.fill(black)
         pygame.draw.circle(screen, (255,255,255), g.ball.get_position(), g.ball.r)
         pygame.draw.rect(screen, (255,255,255), paddle1)
         pygame.draw.rect(screen, (255,255,255), paddle2)
         pygame.display.flip()
-        pygame.time.delay(8)
+        pygame.time.delay(3)
         g.update()
 
 if __name__ == "__main__":
