8df915d0b9acc5d5d7b7df42033aaf1302068229
chany-ahn <chany.kima@gmail.com>
2022-05-08 09:17:02 -0400

    game working, must add game logic

--- src/game.py
+++ src/game.py
@@ -4,18 +4,21 @@ import random
 
 class Game:
 
-    def __init__(self, width : int, height : int, offset=10):
+    def __init__(self, width : int, height : int, offset=10, max_points=10):
         # boundaries of the game board
         self.w = width
         self.h = height
         # score of the two players
-        self.score = (0,0)
+        self.score = [0,0]
+        print(self.score[1])
         # game ball
         self.ball = Ball(self.w//2, self.h//2)
 
         # paddles
         self.paddle1 = Paddle(offset, height//2)
         self.paddle2 = Paddle(width-offset, height//2)
+        self.max_points = max_points
+        
 
     def is_scored(self):
         position = self.ball.get_position()
@@ -41,17 +44,42 @@ class Game:
         if y == r or y == self.h - r:
             self.ball.dy = -self.ball.dy
 
-        # change x cmoponent of the velocity if a collision on right or left
-        if x == r or x == self.w -r:
-            self.ball.dx = -self.ball.dx
+        # # change x cmoponent of the velocity if a collision on right or left
+        if x == r or x == self.w - r:
+            self.ball.set_velocity(0,0)
+            self.point_scored()
+        #     self.ball.dx = -self.ball.dx
+
+    def point_scored(self):
+        print(self.score)
+        x,_ = self.ball.get_position()
+        r = self.ball.r
+
+        if x == r:
+            self.score[0] += 1
+            if self.score[0] == 10:
+                self.game_won()
+            self.reset_game()
+
+        elif x == self.w - r:
+            self.score[1] += 1
+            if self.score[1] == 10:
+                self.game_won()
+            self.reset_game()
+        
+        else:
+            print("Something went horribly wrong")
 
     # function used to reset the game
     def reset_game(self):
         init_velocity = random.choice([[1,1],[1,-1],[-1,1],[-1,-1]])
 
         self.ball.set_velocity(init_velocity[0], init_velocity[1])
+        self.ball.x = self.w//2
+        self.ball.y = self.h//2
 
-        self.score = (0,0)
+        if self.score[0] == self.max_points or self.score[1] == self.max_points:
+            self.score = [0,0]
     
     # movement of each paddle
     def move_paddle1(self, addY):
@@ -81,7 +109,3 @@ class Game:
         self.ball.move()
         self.collision()
 
-
-
-
-g = Game(10, 11)
